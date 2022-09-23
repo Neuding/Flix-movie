c7c130a1988cf2e8908b6ce11323b71c36c1e7a4
Neuding <92402940+Neuding@users.noreply.github.com>
2022-09-22 22:28:04 -0400

    update

--- Pods/Target Support Files/Pods-flix-flixUITests/Pods-flix-flixUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flix_flixUITests : NSObject
-@end
-@implementation PodsDummy_Pods_flix_flixUITests
-@end
---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by CYH on 9/22/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
